1b3557a092adf56481648674668f66c13b8e631d
Jamie F. Olson <self@jfolson.com>
2013-03-21 18:55:42 -0400

    Made functions actually compliant with jQuery instead of doing something totally different.

--- pkg/R/html_attr.R
+++ pkg/R/html_attr.R
@@ -23,7 +23,7 @@ setMethod("rQuery.attr","rQueryResult",function(rquery,...){
     newattrs = list(...)
     if (length(newattrs)>0){
         # Return the "new" elements, which for internal nodes are the same as the original
-        lapply(rquery,.html_attr,value=newattrs)
+        rQueryResult(lapply(rquery,.html_attr,value=newattrs))
     }else {
         .html_attr(rquery[[1]])
     }
--- pkg/R/html_insert.R
+++ pkg/R/html_insert.R
@@ -1,9 +1,8 @@
-
-.html_append = function(elem,value) {
+.html_insert = function(elem,value,...) {
     if (is.character(value)) {
         value <- querySelectorAll(XML:::htmlParse(value,asText=T),"body >*")
     }
-    addChildren(elem,value)
+    addChildren(elem,value,...)
 }
 
 #' Insert content, specified by the parameter, to the end of each element in the 
@@ -18,15 +17,23 @@
 #' @exportMethod rQuery.append
 setGeneric("rQuery.append",function(rquery,value){standardGeneric("rQuery.append")})
 setMethod("rQuery.append","rQueryResult",function(rquery,value){
-          lapply(rquery,.html_append,value=value)
+          rQueryResult(lapply(rquery,.html_insert,value=value))
 })
 
-.html_insert = function(elem,value) {
-    if (is.character(value)) {
-        value <- querySelectorAll(XML:::htmlParse(value,asText=T),"body >*")
-    }
-    addChildren(elem,value,at=0)
-}
+#' Insert content, specified by the parameter, to the beginning of each element in the 
+#' set of matched elements.
+#' 
+#' @param rquery rQueryResult container 
+#' @param value \code{XML} element, \code{rQueryResult} container, or character vector of html  
+#' @returnType rQueryResult 
+#' @return the modified \code{rquery} object
+#' 
+#' @author jfolson
+#' @exportMethod rQuery.prepend
+setGeneric("rQuery.prepend",function(rquery,value){standardGeneric("rQuery.prepend")})
+setMethod("rQuery.prepend","rQueryResult",function(rquery,value){
+          rQueryResult(lapply(rquery,.html_insert,value=value,at=0))
+})
 
 #' Insert content, specified by the parameter, after each element in the set of 
 #' matched elements.
@@ -40,7 +47,11 @@ setMethod("rQuery.append","rQueryResult",function(rquery,value){
 #' @exportMethod rQuery.after
 setGeneric("rQuery.after",function(rquery,value){standardGeneric("rQuery.after")})
 setMethod("rQuery.after","rQueryResult",function(rquery,value){
-          lapply(rquery,.html_append,value=value)
+        xml_parents = lapply(rquery,xmlParent)
+        xml_position = mapply(function(parent,child){
+                which(xmlChildren(parent)==child)-1
+            },xml_parents,rquery)
+        rQueryResult(mapply(.html_insert,xml_parents,value=value,at=xml_position))
 })
 
 #' Insert content, specified by the parameter, before each element in the set of 
@@ -55,7 +66,11 @@ setMethod("rQuery.after","rQueryResult",function(rquery,value){
 #' @exportMethod rQuery.before
 setGeneric("rQuery.before",function(rquery,value){standardGeneric("rQuery.before")})
 setMethod("rQuery.before","rQueryResult",function(rquery,value){
-          lapply(rquery,.html_insert,value=value)
+        xml_parents = lapply(rquery,xmlParent)
+        xml_position = mapply(function(parent,child){
+                which(xmlChildren(parent)==child)
+            },xml_parents,rquery)
+        rQueryResult(mapply(.html_insert,xml_parents,value=value,at=xml_position))
 })
 
 #' Insert every element in the set of matched elements before the target.
@@ -69,7 +84,7 @@ setMethod("rQuery.before","rQueryResult",function(rquery,value){
 #' @exportMethod rQuery.insertBefore
 setGeneric("rQuery.insertBefore",function(rquery,value){standardGeneric("rQuery.insertBefore")})
 setMethod("rQuery.insertBefore","rQueryResult",function(rquery,value){
-    rQueryResult(lapply(rQuery(xmlRoot(rquery[[1]]),value),.html_insert,rquery))
+    rQuery.before(rQuery(xmlRoot(rquery[[1]]),value),rquery)
 })
 
 #' Insert every element in the set of matched elements after the target.
@@ -83,7 +98,7 @@ setMethod("rQuery.insertBefore","rQueryResult",function(rquery,value){
 #' @exportMethod rQuery.insertAfter
 setGeneric("rQuery.insertAfter",function(rquery,value){standardGeneric("rQuery.insertAfter")})
 setMethod("rQuery.insertAfter","rQueryResult",function(rquery,value){
-    rQueryResult(lapply(rQuery(xmlRoot(rquery[[1]]),value),.html_append,rquery))
+    rQuery.after(rQuery(xmlRoot(rquery[[1]]),value),rquery)
 })
 
 
